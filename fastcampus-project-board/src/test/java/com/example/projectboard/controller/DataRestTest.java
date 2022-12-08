dca5cd89ded30845116c5c77a27423b9e6aa3e79
DongHyos <dong87070@naver.com>
2022-12-08 17:35:52 +0900

    #1 - data rest 테스트 수정
    `@Transactional` 애너테이션이 빠져 있어 수정

--- fastcampus-project-board/src/test/java/com/example/projectboard/controller/DataRestTest.java
+++ fastcampus-project-board/src/test/java/com/example/projectboard/controller/DataRestTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("Data REST - API 테스트")
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
 public class DataRestTest {
